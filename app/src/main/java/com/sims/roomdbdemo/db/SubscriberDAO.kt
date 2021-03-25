17dd1e3deddf10b1bffa110e7d1f1fb83b7f20f7
simba_m <scmupfururirwa@gmail.com>
2021-03-25 08:34:57 +0200

    create database instance

--- app/src/main/java/com/sims/roomdbdemo/db/SubscriberDAO.kt
+++ app/src/main/java/com/sims/roomdbdemo/db/SubscriberDAO.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.room.*
 
 @Dao
-interface SubscribeDAO {
+interface SubscriberDAO {
 
     @Insert
     suspend fun insertSubscriber(subscriber: Subscriber): Long
