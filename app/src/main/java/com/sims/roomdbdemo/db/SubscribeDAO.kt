068ae81a46e19846dc4bf3bda7eea0754cf767dd
simba_m <scmupfururirwa@gmail.com>
2021-03-25 08:23:10 +0200

    add more queries

--- app/src/main/java/com/sims/roomdbdemo/db/SubscribeDAO.kt
+++ app/src/main/java/com/sims/roomdbdemo/db/SubscribeDAO.kt
@@ -1,11 +1,23 @@
 package com.sims.roomdbdemo.db
 
-import androidx.room.Dao
-import androidx.room.Insert
+import androidx.lifecycle.LiveData
+import androidx.room.*
 
 @Dao
 interface SubscribeDAO {
 
     @Insert
-    suspend fun insertSubscriber(subscriber: Subscriber)
+    suspend fun insertSubscriber(subscriber: Subscriber): Long
+
+    @Update
+    suspend fun updateSubscriber(subscriber: Subscriber)
+
+    @Delete
+    suspend fun deleteSubscriber(subscriber: Subscriber)
+
+    @Query("DELETE FROM subscriber_data_table")
+    suspend fun deleteAllSubscribers()
+
+    @Query("SELECT * FROM subscriber_data_table")
+    fun getAllSubscribers(): LiveData<List<Subscriber>>
 }
